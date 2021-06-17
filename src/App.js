6516da6793bf01cf64ab90bb99f466efedb0eae4
Barbz547 <barbzval@gmail.com>
2021-06-17 16:35:03 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+// two function components
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+// one class component
+import Footer from './components/Footer';
+
+
+
+export const App = () => {
+
+    const userSearch = (e) => {
+        e.preventDefault();
+        alert(e.body)
+    }
+
+    return (
+        // 
+        <div>
+            <header>
+                <Header user={userSearch} />
+            </header>
+
+            <main>
+                <Main />
+            </main>
+
+            <footer>
+                <Footer />
+            </footer>
+
+        </div>
+    )
+
+}
